468d88d550a784a4c891a4036f136bc9a2ec1540
Davidbenicio <139410907+Davidbenicio@users.noreply.github.com>
2025-11-25 18:19:45 -0300

    Delete project directory

--- project/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Upgrade packaging tools first to prefer wheels over source builds
-python -m pip install --upgrade pip setuptools wheel
-
-# Install requirements preferring binary wheels when available
-python -m pip install --prefer-binary -r project/requirements.txt
